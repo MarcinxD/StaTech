bb80e197f71710fc63182c99153cf28537e39dfc
Marcin Michal Stachula <marcin.stachula@educa.madrid.org>
2023-05-30 00:57:49 +0200

    cartScreen y algo mas

---
+++ client/src/redux/actions/cartAction.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { setLoading, setError, cartItemAdd } from '../slices/cart'
+
+export const addCartItem = (id, qty) => async (dispatch) => {
+    dispatch(setLoading(true));
+    try {
+        const {data} = await axios.get(`/api/products/${id}`);
+        const itemToAdd = {
+            id: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            stock: data.stock,
+            qty,
+        }
+    } catch (error) {
+        dispatch(
+            setError(
+              error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+                ? error.message
+                : 'Se ha producido un error desconocido, pruebe otra vez más tarde.'
+            )
+          );
+    }
+}
